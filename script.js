b675f8fc812efbb44b9b6ff42d1c5e2583bed679
Kazuki-Okazaki <zaki0714is2@gmail.com>
2025-03-04 01:38:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 // デッキ情報（仮のデータ）
-let deck = [
+//let deck = [
 //    { id: 1, name: "カードA", image: "cardA.png" },
 //    { id: 2, name: "カードB", image: "cardB.png" },
 //    { id: 3, name: "カードC", image: "cardC.png" },
 //    { id: 4, name: "カードD", image: "cardD.png" },
 //    { id: 5, name: "カードE", image: "cardE.png" }
-];
+//];
+let deck = {
+    leader: [],
+    main: [],
+    evolve: []
+};
 
 let hand = []; // 手札を管理する配列
 
@@ -21,7 +26,7 @@ updateDeckCount();
 
 // デッキの残り枚数を更新
 function updateDeckCount() {
-    document.getElementById("deck-count").innerText = deck.length;
+    document.getElementById("deck-count").innerText = deck.main.length;
 }
 
 // 手札を更新
@@ -41,12 +46,12 @@ function updateHandDisplay() {
 function drawCard() {
     console.log("デッキの初期状態:", deck);
     updateDeckCount();
-    if (deck.length === 0) {
+    if (deck.main.length === 0) {
         alert("デッキが空です！");
         return;
     }
 
-    let drawnCard = deck.shift();  // デッキの先頭から1枚取り出す
+    let drawnCard = deck.main.shift();  // デッキの先頭から1枚取り出す
     hand.push(drawnCard);  // 手札に追加
     updateDeckCount();  // デッキの残り枚数を更新
     updateHandDisplay(); // 手札の表示を更新
